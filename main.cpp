f5bea01421daec00cabbed9ef843d773a791ef60
Gaurav Kumar <rajputgaurav9582@gmail.com>
2025-06-27 09:24:19 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    int arr[n];
+    for(int i= 0;i<n;i++) cin>> arr[i];
+   // vector<int>res;
+    for(int i = 0;i<n;i++){
+        for(int j = i;j<n;j++){
+            if(arr[i]  >  arr[j])
+            break;
+            else {
+                cout<< arr[j]<< " ";
+            }
+        }
+
+    }
+    return 0;
+}
